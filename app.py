c04cd276382283de01800b1173c7f84f13803d3e
caryyu <cary.yu@wework.com>
2021-05-05 12:10:12 +0800

    add more apis

--- app.py
+++ app.py
@@ -6,13 +6,37 @@ app = Flask(__name__)
 
 @app.route('/search', methods=['GET'])
 def api_search():
+    return api_full_search()
+
+@app.route('/fullsearch', methods=['GET'])
+def api_full_search():
     headers = {'content-type': 'application/json'}
     keyword = request.args.get("q")
     app.logger.info('the parameter is given: %s', keyword)
-    result = func_delegate_try_except_driver(func_keyword_search, str(keyword))
+    result = delegator_try_except_driver(service_keyword_full_search, str(keyword))
     if result:
         return result, 200, headers
     return f'Results Not Found: {keyword}', 404, headers
+
+@app.route('/partialsearch', methods=['GET'])
+def api_partial_search():
+    headers = {'content-type': 'application/json'}
+    keyword = request.args.get("q")
+    app.logger.info('the parameter is given: %s', keyword)
+    result = delegator_try_except_driver(service_keyword_partial_search, str(keyword))
+    if result:
+        return result, 200, headers
+    return f'Results Not Found: {keyword}', 404, headers
+
+@app.route('/fetchbysid', methods=['GET'])
+def api_fetch_by_sid():
+    headers = {'content-type': 'application/json'}
+    sid = request.args.get("sid")
+    app.logger.info('the parameter is given: %s', sid)
+    result = delegator_try_except_driver(service_info_fetch_by_sid, str(sid))
+    if result:
+        return result, 200, headers
+    return f'Results Not Found: {sid}', 404, headers
 
 if __name__ == "__main__":
     app.run()
--- index.py
+++ index.py
@@ -13,7 +13,7 @@ webdriver_options.add_argument("--disable-dev-shm-usage")
 webdriver_options.add_argument("--disable-gpu")
 webdriver_options.add_argument("--no-sandbox")
 
-def func_delegate_try_except_driver(func, string:str):
+def delegator_try_except_driver(func, string:str):
     """
     一个委托函数来初始化 Selenium 的 WebDriver 给目标函数使用
 
@@ -54,6 +54,8 @@ def func_info_fetch(driver:webdriver.Chrome, href:str) -> Dict:
     ranking = driver.find_element_by_css_selector(css_selector="#interest_sectl div.rating_wrap.clearbox div.rating_self.clearfix strong").text
     img = driver.find_element_by_css_selector(css_selector="#mainpic a img").get_attribute(name="src")
     info_text = driver.find_element_by_css_selector(css_selector=".subject #info").text
+    year = re.search("\\((\\d+)\\)", driver.find_element_by_css_selector(css_selector="#content h1 span.year").text).group(1)
+    sid = re.search(".*/(\\d+)/.*", driver.find_element_by_css_selector(css_selector="#mainpic a").get_attribute(name="href")).group(1)
 
     fields_skips = ("^季数:$", "^集数: \\d+$", "^\\d+$")
     fields = ("导演:", "编剧:", "主演:", "类型:", "官方网站:", "制片国家/地区:", "语言:", "上映日期:", "片长:", "又名:", "IMDb链接:")
@@ -65,7 +67,7 @@ def func_info_fetch(driver:webdriver.Chrome, href:str) -> Dict:
     if len(lines) > len(fields):
         raise Exception("Unexpected length: the number of built-in fields aren't greater than expected")
 
-    result:Dict = {"name": name, "ranking": ranking, "img": img}
+    result:Dict = {"name": name, "ranking": ranking, "img": img, "sid": sid, "year": year}
 
     i = 0
     j = 0
@@ -81,7 +83,18 @@ def func_info_fetch(driver:webdriver.Chrome, href:str) -> Dict:
     return result
 
 
-def func_keyword_search(driver:webdriver.Chrome, keyword:str) -> str:
+def service_keyword_full_search(driver:webdriver.Chrome, keyword:str) -> str:
+    """
+    根据 keyword 来查询全部信息 - 该操作为深度检索, 会增加检索的时间
+    注意: 限制的数据条数永远为前 3 条 (减少非相关性数据)
+
+    :Args:
+     - driver - 委托代理往下传递的对象，拥有自动管理关闭的功能
+     - keyword - 给定的关键字字符串
+
+    :Returns:
+     返回 List[Dict] 类型的 JSON 字符串
+    """
     # How many rows should be handled
     limits:int = 3
     driver.get(url="https://www.douban.com")
@@ -93,11 +106,64 @@ def func_keyword_search(driver:webdriver.Chrome, keyword:str) -> str:
     element_result_list:List = driver.find_elements_by_css_selector(css_selector="div.search-result div:nth-child(3) .result")
     result = filter(filter_func_movie_only, element_result_list)
     result = map(map_func_get_href, result)
-    result = map(lambda x: func_delegate_try_except_driver(func_info_fetch, x), list(result)[:limits])
+    result = map(lambda x: delegator_try_except_driver(func_info_fetch, x), list(result)[:limits])
 
     return json.dumps(list(result), ensure_ascii=False)
 
+def service_keyword_partial_search(driver:webdriver.Chrome, keyword:str) -> str:
+    """
+    根据 keyword 来查询列表的部分信息 - 不会进入详情页从而减少响应时间
+    注意: 限制的数据条数永远为前 3 条 (减少非相关性数据)
+
+    :Args:
+     - driver - 委托代理往下传递的对象，拥有自动管理关闭的功能
+     - keyword - 给定的关键字字符串
+
+    :Returns:
+     返回 List[Dict] 类型的 JSON 字符串
+    """
+    # How many rows should be handled
+    limits:int = 3
+    driver.get(url=f"https://www.douban.com/search?q={keyword}")
+    element_result_list:List = driver.find_elements_by_css_selector(css_selector="div.search-result div:nth-child(3) .result")
+    result = filter(filter_func_movie_only, element_result_list)
+
+    def func_item_wrap(element: WebElement) -> Dict:
+        a:WebElement = element.find_element_by_css_selector(css_selector="div.content div h3 a")
+        ranking = element.find_element_by_css_selector(css_selector="div.content div div span.rating_nums").text
+        img = element.find_element_by_css_selector(css_selector="div.pic a img").get_attribute("src")
+        name = a.text
+        sid = re.search(".*sid: (\\d+),.*", a.get_attribute("onclick")).group(1)
+        year = re.search(".*/ (\\d+)$", element.find_element_by_css_selector(css_selector="div.content div div span.subject-cast").text).group(1)
+
+        return {
+            "sid": sid,
+            "name": name.strip(),
+            "ranking": ranking.strip(),
+            "img": img.strip(),
+            "year": year
+        }
+
+    result = map(func_item_wrap, list(result)[:limits])
+    return json.dumps(list(result), ensure_ascii=False)
+
+def service_info_fetch_by_sid(driver:webdriver.Chrome, sid:str) -> str:
+    """
+    根据 sid 来查获取电影详情
+
+    :Args:
+     - driver - 委托代理往下传递的对象，拥有自动管理关闭的功能
+     - sid - 电影的 sid(subject id) 编号，根据此编号查询信息
+
+    :Returns:
+     返回 Dict 类型的 JSON 字符串
+    """
+    result = func_info_fetch(driver=driver, href=f"https://movie.douban.com/subject/{sid}/")
+    return json.dumps(result, ensure_ascii=False)
+
 # if __name__ == "__main__":
-    # result = func_delegate_try_except_driver(func_keyword_search, "aaaaaa")
+    # # result = delegator_try_except_driver(service_keyword_full_search, "Harry Potter")
+    # # result = delegator_try_except_driver(service_keyword_partial_search, "昆虫总动员")
+    # result = delegator_try_except_driver(service_info_fetch_by_sid, "1296996")
     # print(result)
 
