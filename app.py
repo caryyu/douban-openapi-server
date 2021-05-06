2424395cbafc009c2e4d13b2888718049687f247
caryyu <cary.yu@wework.com>
2021-05-06 11:30:03 +0800

    Add flask caching mechanism

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import request
 from index import *
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/search', methods=['GET'])
+@cache.cached(timeout=30)
 def api_search():
     return api_full_search()
 
 @app.route('/fullsearch', methods=['GET'])
+@cache.cached(timeout=30)
 def api_full_search():
     headers = {'content-type': 'application/json'}
     keyword = request.args.get("q")
@@ -19,6 +24,7 @@ def api_full_search():
     return f'Results Not Found: {keyword}', 404, headers
 
 @app.route('/partialsearch', methods=['GET'])
+@cache.cached(timeout=30)
 def api_partial_search():
     headers = {'content-type': 'application/json'}
     keyword = request.args.get("q")
@@ -29,6 +35,7 @@ def api_partial_search():
     return f'Results Not Found: {keyword}', 404, headers
 
 @app.route('/fetchbysid', methods=['GET'])
+@cache.cached(timeout=30)
 def api_fetch_by_sid():
     headers = {'content-type': 'application/json'}
     sid = request.args.get("sid")
