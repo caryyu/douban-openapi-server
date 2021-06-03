040efb1cc0200beb18c964c55b6932f19bebf723
caryyu.tg@gmail.com <caryyu.tg@gmail.com>
2021-06-03 22:22:59 +0800

    keep appending

--- resource/generalapis.py
+++
@@ -1,3 +0,0 @@
-class GeneralAPIs(object):
-
-
--- resource/movie_resource.py
+++ resource/movie_resource.py
@@ -1,20 +1,13 @@
+from resource.base_resource import BaseResource
 from flask_restful import Resource, reqparse
 
-class Movies(Resource):
-    provider = None
-    logger = None
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__()
-        self.provider = kwargs.get('provider')
-        self.logger = kwargs.get('logger')
+class MovieList(BaseResource):
 
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('type', type=str)
         parser.add_argument('q', type=str, required=True)
         params = parser.parse_args()
-        print(params)
         if params['type'] == 'full':
             return self.__api_full_search(params)
         else:
@@ -45,3 +38,35 @@ class Movies(Resource):
         if result:
             return result, 200, headers
         return f'Results Not Found: {keyword}', 404, headers
+
+class Movie(BaseResource):
+    def get(self, sid):
+        return self.__api_fetch_by_sid(sid)
+
+    def __api_fetch_by_sid(self, sid):
+        headers = {'content-type': 'application/json'}
+        self.logger.info('the parameter is given: %s', sid)
+        result = ""
+        try:
+            result = self.provider.fetch_detail_info(str(sid))
+        except Exception as ex:
+            print(ex)
+        if result:
+            return result, 200, headers
+        return f'Results Not Found: {sid}', 404, headers
+
+class MovieCelebrityList(BaseResource):
+    def get(self, sid):
+        return self.__api_fetch_celerities_by_sid(sid)
+
+    def __api_fetch_celerities_by_sid(self, sid):
+        headers = {'content-type': 'application/json'}
+        self.logger.info('the parameter is given: %s', sid)
+        result = ""
+        try:
+            result = self.provider.fetch_celebrities(str(sid))
+        except Exception as ex:
+            print(ex)
+        if result:
+            return result, 200, headers
+        return f'Results Not Found: {sid}', 404, headers
