e0f1e942f6b70fe706a1520da04f104eb4c5c2b8
virualv <virualv@gmail.com>
2022-02-04 16:21:06 +0800

    add some optimize Add: 1. optimize random user-agent 2. add request interval

--- provider/httprequest_provider.py
+++ provider/httprequest_provider.py
@@ -1,39 +1,100 @@
 import json
 import random
 import re
+import time
 from typing import Dict, List
 from bs4 import BeautifulSoup
 import requests
 
 class HttpRequestProvider(object):
-    cookie = {}
 
-    # 添加随机UA伪装
-    headers_arr = [
-        {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
-        },
-        {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
-        },
-        {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76',
-        },
-        {
-            'User-Agent': 'Frodo/07220003 CFNetwork/1240.0.4 Darwin/20.6.0',
-        },
-        {
-            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/97.0.4692.84 Mobile/15E148 Safari/604.1',
-        }
+    # 添加UA列表
+    USER_AGENTS = [
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
+        "Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1467.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.90 Safari/537.36",
+        "Mozilla/5.0 (X11; NetBSD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+        "Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15",
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/97.0.4692.84 Mobile/15E148 Safari/604.1',
+        'Frodo/07220003 CFNetwork/1240.0.4 Darwin/20.6.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.14 (KHTML, like Gecko) Chrome/24.0.1292.0 Safari/537.14"
     ]
-    headers_index = random.randint(0, len(headers_arr) - 1)
-    headers = headers_arr[headers_index]
+
+    """
+    随机UA
+    """
+    def get_random_useragent(self):
+        """生成随机的UserAgent
+        :return: UserAgent字符串
+        """
+        return random.choice(self.USER_AGENTS)
+
+    def get_headers(self):
+        useragent = self.get_random_useragent()
+        headers = {
+            'User-Agent': useragent
+        }
+        return headers
+
+    """
+    生成随机间隔延时
+    """
+    def wait_some_time(self):
+        time.sleep(random.randint(500, 3000) / 3000)
 
     def __init__(self, headers) -> None:
         self.headers = headers
 
     def search_partial_list(self, keyword:str, options: dict) -> List:
-        r = requests.get(f"https://www.douban.com/search?cat=1002&q={keyword}", headers=self.headers)
+        self.wait_some_time()
+        r = requests.get(f"https://www.douban.com/search?cat=1002&q={keyword}", headersheaders=self.get_headers())
         r.raise_for_status()
         soup = BeautifulSoup(r.text, "html.parser")
         elements = soup.select("div.search-result div:nth-child(3) .result")
@@ -65,7 +126,8 @@ class HttpRequestProvider(object):
         return list(result)
 
     def search_full_list(self, keyword:str, options: dict) -> List:
-        r = requests.get(f"https://www.douban.com/search?cat=1002&q={keyword}", headers=self.headers)
+        self.wait_some_time()
+        r = requests.get(f"https://www.douban.com/search?cat=1002&q={keyword}", headers=self.get_headers())
         r.raise_for_status()
         soup = BeautifulSoup(r.text, "html.parser")
         elements = soup.select("div.search-result div:nth-child(3) .result")
@@ -82,6 +144,8 @@ class HttpRequestProvider(object):
 
     def fetch_detail_info(self, sid:str, options: dict) -> Dict:
         r = requests.get(f"https://movie.douban.com/subject/{sid}/", headers=self.headers)
+        self.wait_some_time()
+        r = requests.get(f"https://movie.douban.com/subject/{sid}/", headers=self.get_headers())
         r.raise_for_status()
         soup = BeautifulSoup(r.text, "html.parser")
         title = soup.select_one("#content h1 span:nth-child(1)").string
@@ -118,7 +182,7 @@ class HttpRequestProvider(object):
                 i+=1
 
             if has:
-               result[fields_names[i]] = item[1].strip() 
+               result[fields_names[i]] = item[1].strip()
 
         celebrities = soup.select("ul.celebrities-list li.celebrity")
 
@@ -148,7 +212,8 @@ class HttpRequestProvider(object):
 
     def fetch_celebrities(self, sid:str) -> List:
         limits=15
-        r = requests.get(f"https://movie.douban.com/subject/{sid}/celebrities", headers=self.headers)
+        self.wait_some_time()
+        r = requests.get(f"https://movie.douban.com/subject/{sid}/celebrities", headers=self.get_headers())
         r.raise_for_status()
         soup = BeautifulSoup(r.text, "html.parser")
         elements = soup.select("li.celebrity")
@@ -180,7 +245,8 @@ class HttpRequestProvider(object):
         return list(result)[:limits]
 
     def fetch_celebrity_detail(self, cid) -> dict:
-        r = requests.get(f"https://movie.douban.com/celebrity/{cid}/", headers=self.headers)
+        self.wait_some_time()
+        r = requests.get(f"https://movie.douban.com/celebrity/{cid}/", headers=self.get_headers())
         r.raise_for_status()
         soup = BeautifulSoup(r.text, "html.parser")
         info_text = soup.select_one("#headline div.info ul").get_text()
@@ -211,7 +277,7 @@ class HttpRequestProvider(object):
                 i+=1
 
             if has:
-               result[fields_names[i]] = item[1].strip() 
+               result[fields_names[i]] = item[1].strip()
 
         return result
 
@@ -223,13 +289,14 @@ class HttpRequestProvider(object):
         return "[电影]" == text or "[电视剧]" == text
 
     def fetch_wallpaper(self, sid) -> list[dict]:
-        r = requests.get(f"https://movie.douban.com/subject/{sid}/photos?type=W&start=0&sortby=size&size=a&subtype=a", headers=self.headers)
+        self.wait_some_time()
+        r = requests.get(f"https://movie.douban.com/subject/{sid}/photos?type=W&start=0&sortby=size&size=a&subtype=a", headers=self.get_headers())
         if r.status_code != 200:
             return []
 
         soup = BeautifulSoup(r.text, "html.parser")
         items = soup.select(".poster-col3 li")
-        
+
         result = []
         for item in items:
             data_id = item["data-id"]
@@ -258,7 +325,7 @@ class HttpRequestProvider(object):
             'm': f"https://img2.doubanio.com/view/photo/m/public/p{data_id}.jpg",
             'l': f"https://img2.doubanio.com/view/photo/l/public/p{data_id}.jpg"
         }
-        
+
         return image_dict[image_size]
 
 # if __name__ == "__main__":
@@ -273,4 +340,3 @@ class HttpRequestProvider(object):
 #     # result = p.fetch_celebrities("1295038")
 #     result = json.dumps(result, ensure_ascii=False)
 #     print(result)
-
